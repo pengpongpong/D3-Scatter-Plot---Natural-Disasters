3d4bdc34e288316b61fbe0f8614dc52b8182d6e7
P T <t.m.phuong@hotmail.com>
2022-09-23 00:42:01 +0200

    chart

--- index.js
+++ index.js
@@ -1,40 +1,5 @@
-// import { style } from "d3-selection";
 import { data } from "./data.js";
 
-// const data = [
-//     {
-//         "Year": 2001,
-//         "Death toll": 20085,
-//         "Event": "2001 Gujarat earthquake",
-//         "Countries affected": "India",
-//         "Type": "Earthquake",
-//         "Date": "Jan 26"
-//       },
-//       {
-//         "Year": 2002,
-//         "Death toll": 1030,
-//         "Event": "2002 Indian heat wave",
-//         "Countries affected": "India",
-//         "Type": "Heat wave",
-//         "Date": "May"
-//       },
-//       {
-//         "Year": 2003,
-//         "Death toll": 72000,
-//         "Event": "2003 European heat wave",
-//         "Countries affected": "Europe",
-//         "Type": "Heat wave",
-//         "Date": "July – August"
-//       },
-//       {
-//         "Year": 2004,
-//         "Death toll": 227898,
-//         "Event": "2004 Indian Ocean earthquake and tsunami",
-//         "Countries affected": "Indonesia, Sri Lanka, India, Thailand, Somalia",
-//         "Type": "Earthquake, Tsunami",
-//         "Date": "Dec 26"
-//       }
-// ]
 const margin = {
     top: 20,
     right: 0, 
@@ -42,14 +7,14 @@ const margin = {
     left: 60
 }
 const width = 1000;
-const height = 600;
+const height = 700;
 // axis
 // x = [margin.left, width - margin.right]
 // y = [height - margin.bottom, margin.top]
 
 // y axis scale
 const y = d3.scaleLinear()
-                .domain([d3.min(data, d => d["Death toll"]), d3.max(data, d => d["Death toll"])])
+                .domain([d3.min(data, d => d["Death toll"] - 5000), d3.max(data, d => d["Death toll"])])
                 .range([height - margin.bottom, margin.top]);
 
 // x axis scale
@@ -71,23 +36,29 @@ const tooltip = d3.select("#chart")
                 .style("position", "absolute")
                 .style("box-shadow", "0 0 4px 2px #d3d3d3")
 
-
 const mouseover = function(e) {
     tooltip
         .style("opacity", 1)
     d3.select(this)
         .style("cursor", "crosshair")
-        .style("outline-color", "#00FFAB")
+        .style("outline-style", "solid")
+        .style("outline-color", "black")
+        .style("outline-width", "1px")
+        .style("outline-offset", "1px")
 }
+
+// format number comma for thousand
+const numberFormatter = Intl.NumberFormat("em-US")
+
 const mousemove = function(e) {
     tooltip
         .html(
-            `Event: ${e.toElement.__data__["Event"]}<br>
-            Countries affected: ${e.toElement.__data__["Countries affected"]}<br>
-            Death toll: ${e.toElement.__data__["Death toll"]}<br>
-            Type: ${e.toElement.__data__["Type"]}<br>
-            Date: ${e.toElement.__data__["Date"]}<br>
-            Year: ${e.toElement.__data__["Year"]}<br>
+            `<strong>Event:</strong> ${e.toElement.__data__["Event"]}<br>
+            <strong>Countries affected:</strong> ${e.toElement.__data__["Countries affected"]}<br>
+            <strong>Death toll:</strong> ${numberFormatter.format(e.toElement.__data__["Death toll"])}<br>
+            <strong>Type:</strong> ${e.toElement.__data__["Type"]}<br>
+            <strong>Date:</strong> ${e.toElement.__data__["Date"]}<br>
+            <strong>Year:</strong> ${e.toElement.__data__["Year"]}<br>
             `
             )
         .style("left", `${e.offsetX + 40}px`)
@@ -99,14 +70,14 @@ const mouseleave = function(e) {
         .style("opacity", 0)
     d3.select(this)
         .style("stroke", "none")
-        .style("outline-color", "black")
+        .style("outline-color", "transparent")
 }
 
 // chart
 const svg = d3.select("#chart")
                 .append("svg")
                 .attr("viewbox", [0, 0, width, height])
-                .attr("width", width)
+                .attr("width", "100%")
                 .attr("height", height)
 
 // color for types
@@ -124,11 +95,7 @@ svg.append("g")
     .join("circle")
         .attr("cx", d => x(d["Year"]))
         .attr("cy", d => y(d["Death toll"]))
-        .attr("r", d => 6)
-        .style("outline-style", "solid")
-        .style("outline-color", "black")
-        .style("outline-width", "1px")
-        .style("outline-offset", "2px")
+        .attr("r", d => 7)
         .style("border-radius", "5px")
         .style("fill", ((d, i) => {
             if(d["Type"] === "Heat wave")
@@ -154,20 +121,43 @@ svg.append("g")
 svg.append("g")
     .attr("transform", `translate(0, ${height - margin.bottom})`)
     .attr("font-weight", "bold")
-    .call(d3.axisBottom(x));
+    .call(d3.axisBottom(x).tickSizeOuter(0));
 
 // y-axis
 svg.append("g")
     .attr("transform", `translate(${margin.left}, 0)`)
     .attr("font-weight", "bold")
-    .call(d3.axisLeft(y));
+    .call(d3.axisLeft(y).tickSizeOuter(0));
 
-const yTitle = g => g.append("text")
-                    .attr("font-family", "sans-serif")
-                    .attr("font-size", 10)
-                    .attr("font-weight", "bold")
-                    .attr("y", 20)
-                    .attr("x", 50)
-                    .attr("test")
+const type = ["Earthquake", "Landslide", "Heatwave", "Tsunami", "Flood", "Tropical Cyclone", "Earthquake & Tsunami"]
+const colors = [earthquake, landslide, heatwave, tsunami, flood, tropical, earth_tsunami]
 
-svg.call(yTitle);
+// scale type to color
+let color = d3.scaleOrdinal()
+            .domain(type)
+            .range(colors)
+
+// legend
+const size = 10
+svg.selectAll("legend")
+            .data(type)
+            .enter()     
+            .append("rect")          
+                .attr("x", width - 180)
+                .attr("y", (d, i) => (200 + i*(size + 7)))
+                .attr("width", size)
+                .attr("height", size)
+                .style("fill", d => color(d))
+
+svg.selectAll("legendlabel")
+            .data(type)
+            .enter()
+            .append("text")
+                .attr("x", width - 165)
+                .attr("y", (d, i) => (200 + i*(size + 7) + (size/2)))
+                // .style("fill", d => color(d))
+                .style("fill", "black")
+                .style("font-size", 15)
+                .text(d => d)
+                .attr("text-anchor", "left")
+                .attr("alignment-baseline", "middle")
